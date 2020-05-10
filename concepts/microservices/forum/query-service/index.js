9846277d12dda9dac71659f46aba6b72fb735a35
Marcus Virginia <marcusvirg@yahoo.com>
2020-05-09 21:37:28 -0500

    Added event caching for event bus

--- concepts/microservices/forum/query-service/index.js
+++ concepts/microservices/forum/query-service/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -8,12 +9,8 @@ app.use(cors())
 
 const posts = {}
 
-app.get('/posts', (req, res) => {
-  res.send(posts)
-})
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body
+const handleEvent = ({ type, data }) => {
+  console.log('\nProcessing Event:', type)
   switch (type) {
     case 'POST_CREATED':
       posts[data.id] = {
@@ -38,10 +35,21 @@ app.post('/events', (req, res) => {
       break
     default:
   }
-  console.log(posts)
+}
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/events', (req, res) => {
+  const event = req.body
+  handleEvent(event)
   res.status(200)
 })
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on 4002')
+
+  const res = await axios.get('http://localhost:4005/events')
+  res.data.forEach(event => handleEvent(event))
 })
